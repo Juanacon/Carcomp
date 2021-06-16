145a8e270b87628e950564061953ee73e424649f
Wade Doyle <doylew4@mymacewan.ca>
2021-06-15 22:41:31 -0600

    Welcoepage 3 files sln moved folders

---
+++ CarComp/CarComp/WelcomePage.Designer.cs
@@ -0,0 +1,88 @@
+﻿
+namespace CarComp
+{
+    partial class WelcomePage
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Customer = new System.Windows.Forms.Button();
+            this.Employee = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Customer
+            // 
+            this.Customer.Location = new System.Drawing.Point(196, 222);
+            this.Customer.Name = "Customer";
+            this.Customer.Size = new System.Drawing.Size(94, 29);
+            this.Customer.TabIndex = 0;
+            this.Customer.Text = "Customer";
+            this.Customer.UseVisualStyleBackColor = true;
+            this.Customer.Click += new System.EventHandler(this.Customer_Click);
+            // 
+            // Employee
+            // 
+            this.Employee.Location = new System.Drawing.Point(489, 222);
+            this.Employee.Name = "Employee";
+            this.Employee.Size = new System.Drawing.Size(94, 29);
+            this.Employee.TabIndex = 1;
+            this.Employee.Text = "Employee";
+            this.Employee.UseVisualStyleBackColor = true;
+            this.Employee.Click += new System.EventHandler(this.Employee_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(325, 115);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(104, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Please Choose";
+            // 
+            // WelcomePage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Employee);
+            this.Controls.Add(this.Customer);
+            this.Name = "WelcomePage";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Customer;
+        private System.Windows.Forms.Button Employee;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ CarComp/CarComp/WelcomePage.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CarComp
+{
+    public partial class WelcomePage : Form
+    {
+        public WelcomePage()
+        {
+            InitializeComponent();
+        }
+
+        private void Customer_Click(object sender, EventArgs e)
+        {
+            custlog cl = new custlog();
+            this.Hide();
+            cl.ShowDialog();
+        }
+
+        private void Employee_Click(object sender, EventArgs e)
+        {
+            Employee Em = new Employee();
+            this.Hide();
+            Em.ShowDialog();
+        }
+    }
+}
